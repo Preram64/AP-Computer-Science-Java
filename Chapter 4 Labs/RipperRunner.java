be4140f0ee923d6b711f21bced2688a0e0e738ad
Sean Wangnick <swangnick@gmail.com>
2016-10-17 12:11:35 -0500

    String Ripper Lab
    Added String Ripper Lab

--- Chapter 4 Labs/RipperRunner.java
+++ Chapter 4 Labs/RipperRunner.java
@@ -1,8 +1,8 @@
 //© A+ Computer Science  -  www.apluscompsci.com
-//Name -
-//Date -
-//Class -
-//Lab  - 
+//Name - Sean Wangnick
+//Date - 10/17/16
+//Class - AP Computer Science
+//Lab  - String Ripper
 
 import static java.lang.System.*;
 
@@ -12,23 +12,30 @@ public class RipperRunner
 	{
 		StringRipper demo = new StringRipper("chicken");
 		out.println(demo.ripString(3,6));
-		out.println(demo);
+		out.print(demo);
+		out.println("chicken");
+		out.println("\n\n");
 		
 		demo.setString("alligator");
 		out.println(demo.ripString(3,8));
-		out.println(demo);		
+		out.print(demo);
+		out.println("alligator");
+		out.println("\n\n");
 
 		demo.setString("COMPUTER SCIENCE IS THE BEST!");
 		out.println(demo.ripString(9,12));
 		out.println(demo.ripString(0,12));
 		out.println(demo.ripString(5,20));				
-		out.println(demo);	
+		out.print(demo);
+		out.println("COMPUTER SCIENCE IS THE BEST!");
+		out.println("\n\n");
 
 		demo.setString("I like fried chicken and mashed potatoes!");
 		out.println(demo.ripString(0,7));
 		out.println(demo.ripString(7,15));
 		out.println(demo.ripString(15,26));
 		out.println(demo.ripString(22,26));						
-		out.println(demo);		
+		out.print(demo);
+		out.println("I like fried chicken and mashed potatoes!");
 	}
 }
--- Chapter 4 Labs/StringRipper.java
+++ Chapter 4 Labs/StringRipper.java
@@ -1,8 +1,8 @@
 //© A+ Computer Science  -  www.apluscompsci.com
-//Name -
-//Date -
-//Class -
-//Lab  -
+//Name - Sean Wangnick
+//Date - 10/17/16
+//Class - AP Computer Science
+//Lab  - String Ripper
 
 import static java.lang.System.*;
 
@@ -12,23 +12,26 @@ public class StringRipper
 	
 	public StringRipper()
 	{
+		word = "";
 	}
 
 	public StringRipper(String s)
 	{
+		word = s;
 	}
 	
    public void setString(String s)
    {
+   		word = s;
    }	
 
 	public String ripString(int x, int y)
 	{
-		return "";
+		return word.substring(x, y);
 	}
 
  	public String toString()
  	{
- 		return "\n\n";
+ 		return "";
 	}
 }
