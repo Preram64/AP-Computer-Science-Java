aa03de83dc82113638d3e75d93844353f193973e
Sean Wangnick <swangnick@gmail.com>
2016-11-01 12:25:53 -0500

    Quick Update
    GraphicsRunner, NumberVerifyRunner, StringRunner, StringUtilities

--- Chapter 5 Labs/NumberVerifyRunner.java
+++ Chapter 5 Labs/NumberVerifyRunner.java
@@ -14,31 +14,31 @@ public class NumberVerifyRunner
         //add in input
             Scanner keyboard = new Scanner(System.in);
             
-            System.out.print("Enter a integer  :: ");
+            System.out.print("Enter a whole number  :: ");
             int num = keyboard.nextInt();
         	System.out.println(num + " is odd :: " + NumberVerify.isOdd(num));
         	System.out.println(num + " is even :: " + NumberVerify.isEven(num));
         	System.out.println();
         	
-        	System.out.print("Enter a integer  :: ");
+        	System.out.print("Enter a whole number  :: ");
             num = keyboard.nextInt();
         	System.out.println(num + " is odd :: " + NumberVerify.isOdd(num));
         	System.out.println(num + " is even :: " + NumberVerify.isEven(num));
         	System.out.println();
         	
-        	System.out.print("Enter a integer  :: ");
+        	System.out.print("Enter a whole number  :: ");
             num = keyboard.nextInt();
         	System.out.println(num + " is odd :: " + NumberVerify.isOdd(num));
         	System.out.println(num + " is even :: " + NumberVerify.isEven(num));
         	System.out.println();
         	
-        	System.out.print("Enter a integer  :: ");
+        	System.out.print("Enter a whole number  :: ");
             num = keyboard.nextInt();
         	System.out.println(num + " is odd :: " + NumberVerify.isOdd(num));
         	System.out.println(num + " is even :: " + NumberVerify.isEven(num));
         	System.out.println();
         	
-        	System.out.print("Enter a integer  :: ");
+        	System.out.print("Enter a whole number  :: ");
             num = keyboard.nextInt();
         	System.out.println(num + " is odd :: " + NumberVerify.isOdd(num));
         	System.out.println(num + " is even :: " + NumberVerify.isEven(num));
--- Chapter 5 Labs/StringUtilities.java
+++ Chapter 5 Labs/StringUtilities.java
@@ -3,18 +3,14 @@
 //Date - 10/26/16
 //Lab  - String Ends With
 
-import static java.lang.System.*; 
+import static java.lang.System.*;
 import java.util.Scanner;
 
 public class StringUtilities
 {
  	public static boolean endsWith(String a, String b)
  	{
- 		String one = a;
- 		String two = b;
- 		//String aEnding = indexOf(b);
- 		
- 		if (two.equals (one.substring(one.charAt(5)))) {
+ 		if (b.equals(a.substring(a.length() - b.length()))) {
  			return true;
  		}
  		
